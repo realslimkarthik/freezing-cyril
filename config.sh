012aa864a634da9a4c7b59e27bd23137ca0cb6c8
the-real-slim-Karthik <karthikhariharan13@gmail.com>
2013-02-05 00:16:31 +0530

    Use bash to execute the shell scripts

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 echo "Please provide your password if and when prompted"
-$PKGs=$(dpkg-query -W -f='${Status}\n' sysvbanner|grep "install ok installed")
-if ["$PKGs" == ""]; then
+$PKGs = $(dpkg-query -W --showformat='${Status}\n' sysvbanner|grep "install ok installed")
+
+echo $PKGs
+
+if [ "$PKGs" == "install installed ok" ]; then
 	sudo apt-get install sysvbanner
 else
 	echo "Required packages installed"
