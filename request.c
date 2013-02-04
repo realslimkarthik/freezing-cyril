8747fd2ee3ffdb1d8d8df20128d6a2c687860e8c
the-real-slim-Karthik <karthikhariharan13@gmail.com>
2013-02-04 19:44:48 +0530

    File Sharing Started

---
+++ request.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<string.h>
+
+int main(int argc, char* argv[]) {
+	if(argc != 3) {
+		printf("usage: ./<outfile name> <shared file> <destination ip address>\n");
+	return 0;
+	}
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8000);
+	inet_pton(AF_INET, argv[2], &server.sin_addr);
+	
+	connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr));
+	printf("Connected\n");
+	write(sock, argv[1], sizeof(argv[1]));
+	FILE *f;
+	char *fName = strcat("new_",argv[1]);
+	f = fopen(fName, "w");
+	//f = fopen("abc.html", "w");
+	char line[100];
+	int test = 1;
+	
+	while(test) {
+		test = read(sock, (char *)&line, sizeof(line));
+		printf("%s\n", line);
+		fprintf(f, "%s", line);
+	}
+	fclose(f);
+	close(sock);
+	return 0;
+}
---
+++ sender.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<netinet/in.h>
+
+int main(int argc, char* argv[]) {
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	char line[100];
+	struct sockaddr_in client, server;
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8000);
+	server.sin_addr.s_addr = INADDR_ANY;
+	bind(sock, (struct sockaddr *)&server, sizeof(struct sockaddr));
+	listen(sock, 0);
+	printf("listening\n");
+	int len = sizeof(struct sockaddr);
+	int conn = accept(sock, (struct sockaddr *)&client, &len);
+	printf("Connection accepted\n");
+	read(conn, (char*)&line, sizeof(line));
+	printf("%s\n", line);
+	FILE *f = fopen(line, "r");
+	
+	while(fgets(line, 90, f)) {
+		write(conn, &line, sizeof(line));
+	}
+	
+	fclose(f);
+	close(conn);
+	close(sock);
+	return 0;
+}
